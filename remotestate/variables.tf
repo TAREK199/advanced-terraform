1b1d12ecbbb9284977f41ba006ba01ac484a51a4
rt.tech.54 <rt.tech.54@gmail.com>
2024-02-23 17:05:40 +0100

    deploy all the resources

---
+++ remotestate/variables.tf
@@ -0,0 +1,15 @@
+variable "project-id" {
+  type = string
+  default = "advancedterraform-415009"
+
+}
+
+variable "region" {
+  type = string
+  default = "us-central1"
+}
+
+variable "zone" {
+  type = string
+  default = "us-central1-a"
+}
