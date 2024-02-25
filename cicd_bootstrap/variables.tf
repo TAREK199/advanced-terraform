d123fd88d084ddf631f24122d6294811e6d1fc94
rt.tech.54 <rt.tech.54@gmail.com>
2024-02-25 17:01:08 +0100

    maj

--- cicd_bootstrap/variables.tf
+++ cicd_bootstrap/variables.tf
@@ -1,25 +1,31 @@
 variable "tf_cloud_token" {
+  default = "fpXmPYzn2zsJ5g.atlasv1.EvBWVwHrNLhTdbSzz2LvhQlbzsXTGsy2KmlvJ2r9L9IYZRXfI3Sgz8hXzKxh6YMzYDM"
 
 }
 
 variable "tfcloud_organization" {
+  default = "mrrb"
     
 }
 
 variable "github_oauth_token" {
+  default = "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"
   
 }
 
 variable "dev-project-id" {
   type = string
+  default = "myapp-dev-415315"
 }
 
 variable "qa-project-id" {
   type = string
+  default = "qa-project-415413"
 }
 
 
 variable "vcs-identifier" {
+  default = "TAREK199/advanced-terraform"
   
 }
 
